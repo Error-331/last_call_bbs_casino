6db3e697a13942fe51aa9c35cc8378a39c53a3fe
Sergei <sselihov@brightsign.biz>
2023-01-19 08:21:46 +0200

    Work in progress.

--- src/classes/core/sprite_class.js
+++ src/classes/core/sprite_class.js
@@ -1,5 +1,7 @@
 import Drawable from './drawable_class';
+
 import { findLongestArray } from './../../utils/array_utils';
+import { isNullOrEmpty } from './../../utils/common_utils';
 
 class Sprite extends Drawable {
   #spriteData = [];
@@ -21,6 +23,25 @@ class Sprite extends Drawable {
 
     this.spriteData = spriteData;
     this.#colorMap = colorMap;
+  }
+
+  draw() {
+    for (let rowIdx = 0; rowIdx < this.#spriteData.length; rowIdx++) {
+      const spriteRowData = this.#spriteData[rowIdx];
+      const colorRowData = this.#colorMap[rowIdx];
+
+      for (let symbolIdx = 0; symbolIdx < spriteRowData.length; symbolIdx++) {
+        const symbol = spriteRowData[symbolIdx];
+        const color = isNullOrEmpty(colorRowData[symbolIdx]) ? 0 : colorRowData[symbolIdx];
+
+        this.drawAtLine(symbol, color);
+        this.advanceOneSymbolHorizontal();
+      }
+
+      this.advanceToNextLine();
+    }
+
+    this.reset()
   }
 
   set spriteData(spriteData) {
