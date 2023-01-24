04aabda7144fd924fd85479449be96b87ac69a10
Sergei <sselihov@brightsign.biz>
2023-01-24 08:17:34 +0200

    Add more sprite data.

--- src/classes/slot_machines/base/constants.js
+++ src/classes/slot_machines/base/constants.js
@@ -1,15 +1,46 @@
+import { createFill } from './../../../utils/array_utils';
+
 const BASE_REEL_SYMBOL1 = [
-  '\\▄     ',
-  '▐ \\ ▄  ',
-  ' \\▄(▄) ',
-  ' (▄)   ',
+  ' \\▄      ',
+  ' ▐ \\ ▄   ',
+  '  \\▄(▄)  ',
+  '  (▄)    ',
+];
+
+const BASE_REEL_SYMBOL2 = [
+    '▛▀▀▀▀▀▀▀▜',
+    '▌       ▐',
+    '▌ *BAR* ▐',
+    '▙▄▄▄▄▄▄▄▟',
+];
+
+//const b = ['█','▟','▙','▜','▛','▀','▄','▐','▌','▝','▘','▗','▖'];
+const BASE_REEL_SYMBOL3 = [
+    '   ▄▄▄   ',
+    '  ▌▄  ▐  ',
+    '   / /   ',
+    '  /▄/    '
 ];
 
 const BASE_REEL_SYMBOL1_COLOR_MAP = [
-  [1, 1],
-  [1, 0, 1, 0, 1],
-  [0, 1, 1, 10, 10, 10],
-  [0, 10, 10, 10],
+  [0, 1, 1],
+  [0, 1, 0, 1, 0, 10],
+  [0, 0, 1, 10, 10, 10, 10],
+  [0, 0, 10, 10, 10],
+];
+
+const BASE_REEL_SYMBOL2_COLOR_MAP = [
+    createFill(9, 16),
+    [16, ...createFill(7, 0), 16],
+    [16, 0, ...createFill(5, 17), 0, 16],
+    createFill(9, 16),
+];
+
+const BASE_REEL_SYMBOL3_COLOR_MAP = [
+    createFill(9, 10),
+    createFill(9, 10),
+    createFill(9, 10),
+    createFill(9, 10),
 ];
 
 const BASE_SLOT_MACHINE_UI = [
@@ -57,6 +88,12 @@ _|    \_______________________/    |_
 
 export {
     BASE_REEL_SYMBOL1,
+    BASE_REEL_SYMBOL2,
+    BASE_REEL_SYMBOL3,
+
     BASE_REEL_SYMBOL1_COLOR_MAP,
+    BASE_REEL_SYMBOL2_COLOR_MAP,
+    BASE_REEL_SYMBOL3_COLOR_MAP,
+
     BASE_SLOT_MACHINE_UI,
 }
